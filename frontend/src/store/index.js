aae6e03b20dd81758822bbdd2cfeaa6f922d21f8
ReminaO <oremina@hotmail.fr>
2022-07-10 00:09:48 +0200

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -73,7 +73,6 @@ const store = createStore({
       productId: '',
       username: '',
       content: '',
-
     },
     likes: [],
     likeInfos: {
@@ -81,7 +80,6 @@ const store = createStore({
       productId: '',
       likeState: '',
     },
-    
   },
   plugins: [
     vuejsStorage({
