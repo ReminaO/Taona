1d10c9626053bc43cdf892714363df532a6c9e20
ReminaO <oremina@hotmail.fr>
2022-07-19 20:57:43 +0200

    Configuration de la bannière cookie

--- frontend/src/components/Banner.vue
+++ frontend/src/components/Banner.vue
@@ -7,10 +7,10 @@
                 </p>
             </div>
             <div class="cookies-btn">
-                <button @click="cookieBtn()" class="btn btn-success">
+                <button @click="cookieBtn()" class="btn btn-danger">
                     Accepter les cookies
                 </button>
-                <button @click="cookieBtn()" class="btn btn-deny">
+                <button @click="noCookieBtn()" class="btn btn-deny">
                     Refuser les cookies
                 </button>
             </div>
@@ -20,12 +20,29 @@
 
 <script>
 export default {
+    
     methods: {
         cookieBtn(){
+            //setting cookie for 1 month, cookie will be expired automatically
+            document.cookie="Taona=TaonaCosmetics; Secure; max-age="+60*60*24*30
             const cookies = document.querySelector('.cookies')
-            cookies.style.opacity = "0";
+            if(document.cookie){
+                // cookies.style.opacity = "0"; //hide cookie box once cookie set
+                cookies.classList.add("hide");
+            }else{
+                alert("Cookie can't be set") //If cookie can't be set, an error message occurs
+            }
+        },
+        noCookieBtn(){
+            const cookies = document.querySelector('.cookies')
+            cookies.style.opacity="0"
         }
-    }
+    },
+    mounted(){
+        let checkCookie = document.cookie.indexOf("Taona=TaonaCosmetics")//Checking our set cookie
+        const cookies = document.querySelector('.cookies')
+        checkCookie !=-1 ? cookies.classList.add("hide"):cookies.classList.remove("hide")
+    },
 }
 </script>
 
@@ -60,24 +77,30 @@ body{
     display: flex;
     justify-content: space-between;/*positionner les DIV enfants*/
     align-items: center;/*centrer verticalement*/
-    padding: 3rem;
+    padding: 0.5rem;
     opacity: 1; /* modification de cette valeur en Javascript*/
     transition: 0.5s;
     color:white
+}
+.cookies.hide{
+    opacity:0;
+    visibility:hidden;
+    transition: all 0.3s ease;
 }
 .cookies-btn{
     display: flex;
 }
 .btn{
     color: white;
-    padding: 1rem;
+    padding: 0.5rem;
     border: none;
     border-radius: 10px;
+    margin-right:10px
 }
-.btn.btn-success{
+.btn.btn-danger{
     background-color: #d4a449;
 }
-.btn.btn-success:hover{
+.btn.btn-danger:hover{
     background-color: #b46773;
 }
 .btn.btn-deny{
